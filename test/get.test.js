53a9d68d96fa251b4feabf9bdb12d45121c78486
Justin Lee <justinlee0022@gmail.com>
2017-09-11 17:42:32 -0700

    wrote tests for sliceYear and sliceTerm

--- test/get.test.js
+++ test/get.test.js
@@ -1,2 +1,16 @@
 import assert from 'assert'
 import * as get from '../src/utils/get'
+
+describe('getSliceYear', () => {
+    it('gets the year from the specified term', () => {
+        assert.deepEqual(get.sliceYear('2016W1'), 2016)
+        assert.deepEqual(get.sliceYear('2017W1'), 2017)
+    })
+})
+
+describe('getSliceTerm', () => {
+    it('gets the year from the specified term', () => {
+        assert.deepEqual(get.sliceTerm('2016W1'), 'W1')
+        assert.deepEqual(get.sliceTerm('2017W2'), 'W2')
+    })
+})
