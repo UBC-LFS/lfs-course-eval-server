bbc3819fa1e9a2eda138ad1819524aba10462687
Justin Lee <justinlee0022@gmail.com>
2017-09-13 09:41:03 -0700

    Updated to use env

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import app from './app';
+require('dotenv').config()
 
-const { PORT = 8080 } = process.env;
+const PORT = process.env.PORT
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`)); // eslint-disable-line no-console
