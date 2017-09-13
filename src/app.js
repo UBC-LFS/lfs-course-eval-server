a800ad0be3f50561a04f540160b9c62f423a8714
Justin Lee <justinlee0022@gmail.com>
2017-09-13 12:42:56 -0700

    Added console log of __dirname to see why static assets are not reachable

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ app.use(logger('dev', {
 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+console.log(__dirname)
 app.use(express.static(path.join(__dirname, '../public')));
 
 // Routes
