e2713a97e83e77a05ca7e1ca23658afc0779fc04
Justin Lee <justinlee0022@gmail.com>
2017-09-13 12:46:15 -0700

    added full path.join

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(logger('dev', {
 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-console.log(__dirname)
+console.log(path.join(__dirname, '../public'))
 app.use(express.static(path.join(__dirname, '../public')));
 
 // Routes
