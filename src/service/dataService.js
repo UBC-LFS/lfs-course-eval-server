bb72f4d14c25293488c132a96ef3b04a427b585f
Justin Lee <justinlee0022@gmail.com>
2017-09-20 18:55:19 -0700

    refactored, sending filtered data back to client correctly

--- src/service/dataService.js
+++ src/service/dataService.js
@@ -4,13 +4,7 @@ import readCSV from './readCSV'
 import R from 'ramda'
 
 const filterDataByFilterSettings = ({ chartKey, year, term, courseNum, department, toggleBelowMin, classSizeMin, classSizeMax }) => {
-    // what to do with chartkey?
-    console.log(chartKey, year, term, courseNum, department, toggleBelowMin, classSizeMin, classSizeMax)
-    // filter according to params sent by client
-    const filterPipeline = (data) => {
-        // filters go in here
-        // TODO: fix filter function
-        const filterData = R.pipe(
+    const filterPipeline = (data) => R.pipe(
             filter.byYear(year),
             filter.byTerm(get.sliceTerm(term)),
             filter.byCourseNum(courseNum),
@@ -18,9 +12,6 @@ const filterDataByFilterSettings = ({ chartKey, year, term, courseNum, departmen
             filter.byClassSize(classSizeMin, classSizeMax),
             filter.byToggleBelowMin(toggleBelowMin)
         )(data)
-        console.log(filterData)
-        return filterData
-    }
 
     return new Promise((resolve, reject) => {
         // specify what file or eventually DB to connect to
