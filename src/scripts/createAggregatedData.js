571645c953c6fdc56a364b930863107f478767d7
Justin Lee <justinlee0022@gmail.com>
2017-09-26 09:31:17 -0700

    Created mockCourseStructure, which is what a course evaluation will look like when we store it in mongo

---
+++ src/scripts/createAggregatedData.js
@@ -0,0 +1,14 @@
+import fs from 'fs'
+import parse from 'csv-parse'
+import * as calculate from '../utils/calculate'
+
+const readCSV = (filename, callback) => {
+    const parser = parse({delimiter: ',', columns: true, relax: true, auto_parse: true}, (
+        err, data) => {
+            if (err) throw err
+            callback(data)
+    })
+    fs.createReadStream(__dirname + '/source/' + filename).pipe(parser)
+}
+
+readCSV('mockRawData.csv', (x) => console.log(x))
