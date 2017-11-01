04aa2e8a6a560aa58245255e4fad05eedd62b09c
Justin Lee <justinlee0022@gmail.com>
2017-11-01 11:31:28 -0700

    creating object that holds computed historical faculty results. now going to implement dept averages

--- src/scripts/createFacultyDataCollection.js
+++ src/scripts/createFacultyDataCollection.js
@@ -3,33 +3,38 @@ import * as filterCSV from './scriptUtils/filterCSV'
 import R from 'ramda'
 import readCSV from '../service/readCSV'
 
+const calculateAverageOfFaculty = (filteredArray) => {
+  const UMI1 = filterCSV.byUMI1(filteredArray)
+  const UMI2 = filterCSV.byUMI2(filteredArray)
+  const UMI3 = filterCSV.byUMI3(filteredArray)
+  const UMI4 = filterCSV.byUMI4(filteredArray)
+  const UMI5 = filterCSV.byUMI5(filteredArray)
+  const UMI6 = filterCSV.byUMI6(filteredArray)
+
+  const averageUMI1 = R.mean(UMI1)
+  const averageUMI2 = R.mean(UMI2)
+  const averageUMI3 = R.mean(UMI3)
+  const averageUMI4 = R.mean(UMI4)
+  const averageUMI5 = R.mean(UMI5)
+  const averageUMI6 = R.mean(UMI6)
+
+  return ({ UMI1: averageUMI1, UMI2: averageUMI2, UMI3: averageUMI3, UMI4: averageUMI4, UMI5: averageUMI5, UMI6: averageUMI6 })
+}
+
 const facultyAverageByYear = (csv) => {
   const uniqYears = getFromCSV.getUniqYears(csv)
   const averageUMIByYear = []
 
   uniqYears.map(year => {
     const filteredByYear = filterCSV.byYear(year)(csv)
-
-    const UMI1 = filterCSV.byUMI1(filteredByYear)
-    const UMI2 = filterCSV.byUMI2(filteredByYear)
-    const UMI3 = filterCSV.byUMI3(filteredByYear)
-    const UMI4 = filterCSV.byUMI4(filteredByYear)
-    const UMI5 = filterCSV.byUMI5(filteredByYear)
-    const UMI6 = filterCSV.byUMI6(filteredByYear)
-
-    const averageUMI1 = R.mean(UMI1)
-    const averageUMI2 = R.mean(UMI2)
-    const averageUMI3 = R.mean(UMI3)
-    const averageUMI4 = R.mean(UMI4)
-    const averageUMI5 = R.mean(UMI5)
-    const averageUMI6 = R.mean(UMI6)
-
     averageUMIByYear.push({
-      facultyAverage: { year, UMI1: averageUMI1, UMI2: averageUMI2, UMI3: averageUMI3, UMI4: averageUMI4, UMI5: averageUMI5, UMI6: averageUMI6 }
+      [year]: {
+        facultyAverage: calculateAverageOfFaculty(filteredByYear)
+      }
     })
   })
 
-  console.log(averageUMIByYear)
+  console.log(JSON.stringify(averageUMIByYear, null, 2))
 }
 
 readCSV('../scripts/source/rawDataAll.csv', (csv) => {
