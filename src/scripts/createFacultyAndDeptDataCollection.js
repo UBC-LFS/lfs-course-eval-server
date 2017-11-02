c66471a54ee8b129229cd90eef36bef25a32f90a
Justin Lee <justinlee0022@gmail.com>
2017-11-02 15:25:02 -0700

    removed unused console

--- src/scripts/createFacultyAndDeptDataCollection.js
+++ src/scripts/createFacultyAndDeptDataCollection.js
@@ -19,7 +19,6 @@ const calculateAverage = (filteredArray) => {
   const averageUMI5 = R.mean(UMI5)
   const averageUMI6 = R.mean(UMI6)
 
-  // console.log(UMI1.length, UMI2.length, UMI3.length, UMI4.length, UMI5.length, UMI6.length)
   const averageLength = Math.round(R.mean([UMI1.length, UMI2.length, UMI3.length, UMI4.length, UMI5.length, UMI6.length]))
 
   return ({
