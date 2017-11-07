f9e9a1f1e7eb1c598685c4613e28147c5e1530f7
clarachu <cgc.chu@gmail.com>
2017-11-07 12:26:44 -0800

    CC - rename tuple[0],tuple[1]

--- src/scripts/createOverallInstructorCollection.js
+++ src/scripts/createOverallInstructorCollection.js
@@ -2,53 +2,54 @@ import { readDataByYear, writeToDB, clearCollection } from '../service/dbService
 import R from 'ramda'
 import * as calculate from '../utils/calculate'
 
-const sumCount = (umi, val, tuple) =>
-  R.reduce((acc, record) => (acc + record[umi].count[val]), 0, tuple[1])
+const sumCount = (umi, val, instructorRecord) =>
+  R.reduce((acc, record) => (acc + record[umi].count[val]), 0, instructorRecord[1])
 
-const sumGender = (gen, tuple) =>
-  R.reduce((acc, record) => (acc + record.gender[gen]), 0, tuple[1])
+const sumGender = (gen, instructorRecord) =>
+  R.reduce((acc, record) => (acc + record.gender[gen]), 0, instructorRecord[1])
 
-const sumEnrolment = (tuple) =>
-  R.reduce((acc, record) => (acc + record.enrolment), 0, tuple[1])
+const sumEnrolment = (instructorRecord) =>
+  R.reduce((acc, record) => (acc + record.enrolment), 0, instructorRecord[1])
 
-const sumResponded = (tuple) =>
-  R.reduce((acc, record) => (acc + (record.responseRate * record.enrolment)), 0, tuple[1])
+const sumResponded = (instructorRecord) =>
+  R.reduce((acc, record) => (acc + (record.responseRate * record.enrolment)), 0, instructorRecord[1])
 
-const sumCourseCount = (tuple) =>
-  R.reduce((acc, record) => (acc + 1), 0, tuple[1])
+const sumCourseCount = (instructorRecord) =>
+  R.reduce((acc, record) => (acc + 1), 0, instructorRecord[1])
 
-const concatenateDept = (tuple) => {
+const concatenateDept = (instructorRecord) => {
   const deptList = []
   R.map(x => {
     if (!deptList.includes(x.dept)) { deptList.push(x.dept) }
-  }, tuple[1])
+  }, instructorRecord[1])
   return deptList
 }
 
 const aggregateOverallInstructor = (data) => {
   const byInstructor = R.groupBy((course) => course.PUID)
 
-  const result = R.reduce((acc, tuple) => {
+  const result = R.reduce((acc, instructorRecord) => {
+      const classes = instructorRecord[1]
       const instructorObj = {
-      instructorName: tuple[1][0].instructorName,
+      instructorName: classes[0].instructorName,
       gender: {
-        Female: sumGender('Female', tuple),
-        Male: sumGender('Male', tuple)
+        Female: sumGender('Female', instructorRecord),
+        Male: sumGender('Male', instructorRecord)
       },
-      numCoursesTaught: tuple[1].length,
-      numStudentsTaught: sumEnrolment(tuple),
-      responseRate: sumResponded(tuple) / sumEnrolment(tuple),
-      dept: concatenateDept(tuple).join(', ')
+      numCoursesTaught: classes.length,
+      numStudentsTaught: sumEnrolment(instructorRecord),
+      responseRate: sumResponded(instructorRecord) / sumEnrolment(instructorRecord),
+      dept: concatenateDept(instructorRecord).join(', ')
     }
 
     for (let i = 1; i <= 6; i++) {
       instructorObj['UMI' + i] = {
         count: {
-          '1': sumCount('UMI' + i, '1', tuple),
-          '2': sumCount('UMI' + i, '2', tuple),
-          '3': sumCount('UMI' + i, '3', tuple),
-          '4': sumCount('UMI' + i, '4', tuple),
-          '5': sumCount('UMI' + i, '5', tuple)
+          '1': sumCount('UMI' + i, '1', instructorRecord),
+          '2': sumCount('UMI' + i, '2', instructorRecord),
+          '3': sumCount('UMI' + i, '3', instructorRecord),
+          '4': sumCount('UMI' + i, '4', instructorRecord),
+          '5': sumCount('UMI' + i, '5', instructorRecord)
         }
       }
       instructorObj['UMI' + i]['dispersionIndex'] = calculate.dispersionIndex(instructorObj['UMI' + i].count)
