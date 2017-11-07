9b2f3232745f2f90812ece28bf4841b3ef05f649
Justin Lee <justinlee0022@gmail.com>
2017-11-07 15:57:43 -0800

    commented out irrelevant dataservice test for now

--- test/dataService.test.js
+++ test/dataService.test.js
@@ -8,15 +8,15 @@ describe('filterDataByFilterSettings', () => {
   })
 })
 
-describe('filterData', () => {
-  it('creates a filter object according to the data in the mockAggregatedData', () => {
-    const output = {
-      years: [2016],
-      terms: ['W1', 'W2'],
-      courseLevels: [100],
-      questionCodes: ['UMI1', 'UMI2', 'UMI3', 'UMI4', 'UMI5', 'UMI6'],
-      depts: ['LFS']
-    }
-    return DS.filterData().then(result => assert.deepEqual(result, output))
-  })
-})
+// describe('filterData', () => {
+//   it('creates a filter object according to the data in the mockAggregatedData', () => {
+//     const output = {
+//       years: [2016],
+//       terms: ['W1', 'W2'],
+//       courseLevels: [100],
+//       questionCodes: ['UMI1', 'UMI2', 'UMI3', 'UMI4', 'UMI5', 'UMI6'],
+//       depts: ['LFS']
+//     }
+//     return DS.filterData().then(result => assert.deepEqual(result, output))
+//   })
+// })
