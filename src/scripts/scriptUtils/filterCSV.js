4048471dc5a49c94d5c19d48d97546df0391f6f3
Justin Lee <justinlee0022@gmail.com>
2017-11-14 15:28:47 -0800

    refactored filterCSV

--- src/scripts/scriptUtils/filterCSV.js
+++ src/scripts/scriptUtils/filterCSV.js
@@ -7,12 +7,12 @@ const byDept = (dept) => R.filter(x => x.deptname === dept)
 
 const invalidResults = R.filter(x => x === 1 || x === 2 || x === 3 || x === 4 || x === 5)
 
-const byUMI1 = (csv) => invalidResults(R.map(x => getFromCSV.getUMI1(x), csv))
-const byUMI2 = (csv) => invalidResults(R.map(x => getFromCSV.getUMI2(x), csv))
-const byUMI3 = (csv) => invalidResults(R.map(x => getFromCSV.getUMI3(x), csv))
-const byUMI4 = (csv) => invalidResults(R.map(x => getFromCSV.getUMI4(x), csv))
-const byUMI5 = (csv) => invalidResults(R.map(x => getFromCSV.getUMI5(x), csv))
-const byUMI6 = (csv) => invalidResults(R.map(x => getFromCSV.getUMI6(x), csv))
+const byUMI1 = (csv) => invalidResults(csv.map(x => getFromCSV.getUMI1(x)))
+const byUMI2 = (csv) => invalidResults(csv.map(x => getFromCSV.getUMI2(x)))
+const byUMI3 = (csv) => invalidResults(csv.map(x => getFromCSV.getUMI3(x)))
+const byUMI4 = (csv) => invalidResults(csv.map(x => getFromCSV.getUMI4(x)))
+const byUMI5 = (csv) => invalidResults(csv.map(x => getFromCSV.getUMI5(x)))
+const byUMI6 = (csv) => invalidResults(csv.map(x => getFromCSV.getUMI6(x)))
 
 export {
   byDept,
