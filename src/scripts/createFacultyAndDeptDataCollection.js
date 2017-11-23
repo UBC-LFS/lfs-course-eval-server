442b08658475adf56d35ef89820185223b58e11e
Justin Lee <justinlee0022@gmail.com>
2017-11-23 11:14:23 -0800

    calculated averages

--- src/scripts/createFacultyAndDeptDataCollection.js
+++ src/scripts/createFacultyAndDeptDataCollection.js
@@ -1,8 +1,7 @@
 import * as filterCSV from './scriptUtils/filterCSV'
 import R from 'ramda'
-import readCSV from '../service/readCSV'
 import { readDataByYear, writeToDB, clearCollection } from '../service/dbService'
-import { join } from 'path';
+import { umiAvg } from '../utils/calculate'
 
 const calculateAverage = (filteredArray) => {
   const UMI1 = filterCSV.byUMI1(filteredArray)
@@ -40,56 +39,65 @@ const createAverageByYear = (data) => {
     ).filter(arr => arr.length > 0)
   )
 
-  console.log(JSON.stringify(groupedByDeptsThenYearAndTerm, null, 2))
-
   groupedByDeptsThenYearAndTerm.map(depts => {
     depts.map(yearAndTerm => {
       const obj = yearAndTerm.reduce((acc, cur) => {
-        acc['UMI1'] = acc['UMI1'] || cur['UMI1'].count
-        acc['UMI2'] = acc['UMI2'] || cur['UMI2'].count
-        acc['UMI3'] = acc['UMI3'] || cur['UMI3'].count
-        acc['UMI4'] = acc['UMI4'] || cur['UMI4'].count
-        acc['UMI5'] = acc['UMI5'] || cur['UMI5'].count
-        acc['UMI6'] = acc['UMI6'] || cur['UMI6'].count
+        const tempObj = {}
 
-        acc['UMI1']['1'] = acc['UMI1']['1'] + cur['UMI1'].count['1'] || cur['UMI1'].count['1']
-        acc['UMI1']['2'] = acc['UMI1']['2'] + cur['UMI1'].count['2'] || cur['UMI1'].count['2']
-        acc['UMI1']['3'] = acc['UMI1']['3'] + cur['UMI1'].count['3'] || cur['UMI1'].count['3']
-        acc['UMI1']['4'] = acc['UMI1']['4'] + cur['UMI1'].count['4'] || cur['UMI1'].count['4']
-        acc['UMI1']['5'] = acc['UMI1']['5'] + cur['UMI1'].count['5'] || cur['UMI1'].count['5']
+        tempObj['UMI1'] = tempObj['UMI1'] || cur['UMI1'].count
+        tempObj['UMI2'] = tempObj['UMI2'] || cur['UMI2'].count
+        tempObj['UMI3'] = tempObj['UMI3'] || cur['UMI3'].count
+        tempObj['UMI4'] = tempObj['UMI4'] || cur['UMI4'].count
+        tempObj['UMI5'] = tempObj['UMI5'] || cur['UMI5'].count
+        tempObj['UMI6'] = tempObj['UMI6'] || cur['UMI6'].count
 
-        acc['UMI2']['1'] = acc['UMI1']['1'] + cur['UMI1'].count['1'] || cur['UMI1'].count['1']
-        acc['UMI2']['2'] = acc['UMI1']['2'] + cur['UMI1'].count['2'] || cur['UMI1'].count['2']
-        acc['UMI2']['3'] = acc['UMI1']['3'] + cur['UMI1'].count['3'] || cur['UMI1'].count['3']
-        acc['UMI2']['4'] = acc['UMI1']['4'] + cur['UMI1'].count['4'] || cur['UMI1'].count['4']
-        acc['UMI2']['5'] = acc['UMI1']['5'] + cur['UMI1'].count['5'] || cur['UMI1'].count['5']
+        tempObj['UMI1']['1'] = tempObj['UMI1']['1'] + cur['UMI1'].count['1'] || cur['UMI1'].count['1']
+        tempObj['UMI1']['2'] = tempObj['UMI1']['2'] + cur['UMI1'].count['2'] || cur['UMI1'].count['2']
+        tempObj['UMI1']['3'] = tempObj['UMI1']['3'] + cur['UMI1'].count['3'] || cur['UMI1'].count['3']
+        tempObj['UMI1']['4'] = tempObj['UMI1']['4'] + cur['UMI1'].count['4'] || cur['UMI1'].count['4']
+        tempObj['UMI1']['5'] = tempObj['UMI1']['5'] + cur['UMI1'].count['5'] || cur['UMI1'].count['5']
 
-        acc['UMI3']['1'] = acc['UMI1']['1'] + cur['UMI1'].count['1'] || cur['UMI1'].count['1']
-        acc['UMI3']['2'] = acc['UMI1']['2'] + cur['UMI1'].count['2'] || cur['UMI1'].count['2']
-        acc['UMI3']['3'] = acc['UMI1']['3'] + cur['UMI1'].count['3'] || cur['UMI1'].count['3']
-        acc['UMI3']['4'] = acc['UMI1']['4'] + cur['UMI1'].count['4'] || cur['UMI1'].count['4']
-        acc['UMI3']['5'] = acc['UMI1']['5'] + cur['UMI1'].count['5'] || cur['UMI1'].count['5']
+        tempObj['UMI2']['1'] = tempObj['UMI2']['1'] + cur['UMI2'].count['1'] || cur['UMI2'].count['1']
+        tempObj['UMI2']['2'] = tempObj['UMI2']['2'] + cur['UMI2'].count['2'] || cur['UMI2'].count['2']
+        tempObj['UMI2']['3'] = tempObj['UMI2']['3'] + cur['UMI2'].count['3'] || cur['UMI2'].count['3']
+        tempObj['UMI2']['4'] = tempObj['UMI2']['4'] + cur['UMI2'].count['4'] || cur['UMI2'].count['4']
+        tempObj['UMI2']['5'] = tempObj['UMI2']['5'] + cur['UMI2'].count['5'] || cur['UMI2'].count['5']
 
-        acc['UMI4']['1'] = acc['UMI1']['1'] + cur['UMI1'].count['1'] || cur['UMI1'].count['1']
-        acc['UMI4']['2'] = acc['UMI1']['2'] + cur['UMI1'].count['2'] || cur['UMI1'].count['2']
-        acc['UMI4']['3'] = acc['UMI1']['3'] + cur['UMI1'].count['3'] || cur['UMI1'].count['3']
-        acc['UMI4']['4'] = acc['UMI1']['4'] + cur['UMI1'].count['4'] || cur['UMI1'].count['4']
-        acc['UMI4']['5'] = acc['UMI1']['5'] + cur['UMI1'].count['5'] || cur['UMI1'].count['5']
+        tempObj['UMI3']['1'] = tempObj['UMI3']['1'] + cur['UMI3'].count['1'] || cur['UMI3'].count['1']
+        tempObj['UMI3']['2'] = tempObj['UMI3']['2'] + cur['UMI3'].count['2'] || cur['UMI3'].count['2']
+        tempObj['UMI3']['3'] = tempObj['UMI3']['3'] + cur['UMI3'].count['3'] || cur['UMI3'].count['3']
+        tempObj['UMI3']['4'] = tempObj['UMI3']['4'] + cur['UMI3'].count['4'] || cur['UMI3'].count['4']
+        tempObj['UMI3']['5'] = tempObj['UMI3']['5'] + cur['UMI3'].count['5'] || cur['UMI3'].count['5']
 
-        acc['UMI5']['1'] = acc['UMI1']['1'] + cur['UMI1'].count['1'] || cur['UMI1'].count['1']
-        acc['UMI5']['2'] = acc['UMI1']['2'] + cur['UMI1'].count['2'] || cur['UMI1'].count['2']
-        acc['UMI5']['3'] = acc['UMI1']['3'] + cur['UMI1'].count['3'] || cur['UMI1'].count['3']
-        acc['UMI5']['4'] = acc['UMI1']['4'] + cur['UMI1'].count['4'] || cur['UMI1'].count['4']
-        acc['UMI5']['5'] = acc['UMI1']['5'] + cur['UMI1'].count['5'] || cur['UMI1'].count['5']
+        tempObj['UMI4']['1'] = tempObj['UMI4']['1'] + cur['UMI4'].count['1'] || cur['UMI4'].count['1']
+        tempObj['UMI4']['2'] = tempObj['UMI4']['2'] + cur['UMI4'].count['2'] || cur['UMI4'].count['2']
+        tempObj['UMI4']['3'] = tempObj['UMI4']['3'] + cur['UMI4'].count['3'] || cur['UMI4'].count['3']
+        tempObj['UMI4']['4'] = tempObj['UMI4']['4'] + cur['UMI4'].count['4'] || cur['UMI4'].count['4']
+        tempObj['UMI4']['5'] = tempObj['UMI4']['5'] + cur['UMI4'].count['5'] || cur['UMI4'].count['5']
 
-        acc['UMI6']['1'] = acc['UMI1']['1'] + cur['UMI1'].count['1'] || cur['UMI1'].count['1']
-        acc['UMI6']['2'] = acc['UMI1']['2'] + cur['UMI1'].count['2'] || cur['UMI1'].count['2']
-        acc['UMI6']['3'] = acc['UMI1']['3'] + cur['UMI1'].count['3'] || cur['UMI1'].count['3']
-        acc['UMI6']['4'] = acc['UMI1']['4'] + cur['UMI1'].count['4'] || cur['UMI1'].count['4']
-        acc['UMI6']['5'] = acc['UMI1']['5'] + cur['UMI1'].count['5'] || cur['UMI1'].count['5']
+        tempObj['UMI5']['1'] = tempObj['UMI5']['1'] + cur['UMI5'].count['1'] || cur['UMI5'].count['1']
+        tempObj['UMI5']['2'] = tempObj['UMI5']['2'] + cur['UMI5'].count['2'] || cur['UMI5'].count['2']
+        tempObj['UMI5']['3'] = tempObj['UMI5']['3'] + cur['UMI5'].count['3'] || cur['UMI5'].count['3']
+        tempObj['UMI5']['4'] = tempObj['UMI5']['4'] + cur['UMI5'].count['4'] || cur['UMI5'].count['4']
+        tempObj['UMI5']['5'] = tempObj['UMI5']['5'] + cur['UMI5'].count['5'] || cur['UMI5'].count['5']
+
+        tempObj['UMI6']['1'] = tempObj['UMI6']['1'] + cur['UMI6'].count['1'] || cur['UMI6'].count['1']
+        tempObj['UMI6']['2'] = tempObj['UMI6']['2'] + cur['UMI6'].count['2'] || cur['UMI6'].count['2']
+        tempObj['UMI6']['3'] = tempObj['UMI6']['3'] + cur['UMI6'].count['3'] || cur['UMI6'].count['3']
+        tempObj['UMI6']['4'] = tempObj['UMI6']['4'] + cur['UMI6'].count['4'] || cur['UMI6'].count['4']
+        tempObj['UMI6']['5'] = tempObj['UMI6']['5'] + cur['UMI6'].count['5'] || cur['UMI6'].count['5']
+
+        acc['UMI1Avg'] = umiAvg(tempObj['UMI1'])
+        acc['UMI2Avg'] = umiAvg(tempObj['UMI2'])
+        acc['UMI3Avg'] = umiAvg(tempObj['UMI3'])
+        acc['UMI4Avg'] = umiAvg(tempObj['UMI4'])
+        acc['UMI5Avg'] = umiAvg(tempObj['UMI5'])
+        acc['UMI6Avg'] = umiAvg(tempObj['UMI6'])
+
+        acc['length'] = acc['length'] + 1 || 1
         return acc
       }, {})
-      console.log(obj)
+      // console.log(obj)
     })
   })
   // const result = []
