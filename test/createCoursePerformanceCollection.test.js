116d6c1378833f0737a7b2a8fe29e3f525583cc1
Justin Lee <justinlee0022@gmail.com>
2017-11-28 12:02:18 -0800

    Added another test for createCoursePerformanceCollection

--- test/createCoursePerformanceCollection.test.js
+++ test/createCoursePerformanceCollection.test.js
@@ -81,4 +81,118 @@ describe('addDeptData', () => {
     ]
     assert.deepEqual(addDeptData(courses, deptAvg), output)
   })
+
+  it('should work for more than one course at a time', () => {
+    let courses = [
+      {
+        year: 2014,
+        term: 'W2',
+        course: 'DEF 123',
+        section: '001',
+        dept: 'APBI',
+        instructorName: 'Fake Name 2',
+        PUID: 12345678,
+        gender: {
+          Female: 5,
+          Male: 5
+        },
+        enrolment: 12,
+        responseRate: 0.8,
+        UMI1: { count: { '1': 0, '2': 2, '3': 3, '4': 7, '5': 5 } },
+        UMI2: { count: { '1': 0, '2': 2, '3': 3, '4': 7, '5': 5 } },
+        UMI3: { count: { '1': 0, '2': 2, '3': 3, '4': 7, '5': 5 } },
+        UMI4: { count: { '1': 0, '2': 2, '3': 3, '4': 7, '5': 5 } },
+        UMI5: { count: { '1': 0, '2': 2, '3': 3, '4': 7, '5': 5 } },
+        UMI6: { count: { '1': 0, '2': 2, '3': 3, '4': 7, '5': 5 } }
+      },
+      {
+        year: 2014,
+        term: 'W2',
+        course: 'ABC 123',
+        section: '001',
+        dept: 'APBI',
+        instructorName: 'Fake Name',
+        PUID: 12345678,
+        gender: {
+          Female: 5,
+          Male: 5
+        },
+        enrolment: 12,
+        responseRate: 0.8,
+        UMI1: { count: { '1': 0, '2': 2, '3': 3, '4': 7, '5': 5 } },
+        UMI2: { count: { '1': 0, '2': 2, '3': 3, '4': 7, '5': 5 } },
+        UMI3: { count: { '1': 0, '2': 2, '3': 3, '4': 7, '5': 5 } },
+        UMI4: { count: { '1': 0, '2': 2, '3': 3, '4': 7, '5': 5 } },
+        UMI5: { count: { '1': 0, '2': 2, '3': 3, '4': 7, '5': 5 } },
+        UMI6: { count: { '1': 0, '2': 2, '3': 3, '4': 7, '5': 5 } }
+      }
+    ]
+    let output = [
+      {
+        year: 2014,
+        term: 'W2',
+        course: 'DEF 123',
+        section: '001',
+        dept: 'APBI',
+        instructorName: 'Fake Name 2',
+        PUID: 12345678,
+        gender: {
+          Female: 5,
+          Male: 5
+        },
+        enrolment: 12,
+        responseRate: 0.8,
+        UMI1: { count: { '1': 0, '2': 2, '3': 3, '4': 7, '5': 5 } },
+        UMI2: { count: { '1': 0, '2': 2, '3': 3, '4': 7, '5': 5 } },
+        UMI3: { count: { '1': 0, '2': 2, '3': 3, '4': 7, '5': 5 } },
+        UMI4: { count: { '1': 0, '2': 2, '3': 3, '4': 7, '5': 5 } },
+        UMI5: { count: { '1': 0, '2': 2, '3': 3, '4': 7, '5': 5 } },
+        UMI6: { count: { '1': 0, '2': 2, '3': 3, '4': 7, '5': 5 } },
+        deptAverage: {
+          UMI1: 1.23,
+          UMI2: 1.23,
+          UMI3: 1.23,
+          UMI4: 1.23,
+          UMI5: 1.23,
+          UMI6: 1.23,
+          year: 2014,
+          term: 'W2',
+          length: 41
+        }
+      },
+      {
+        year: 2014,
+        term: 'W2',
+        course: 'ABC 123',
+        section: '001',
+        dept: 'APBI',
+        instructorName: 'Fake Name',
+        PUID: 12345678,
+        gender: {
+          Female: 5,
+          Male: 5
+        },
+        enrolment: 12,
+        responseRate: 0.8,
+        UMI1: { count: { '1': 0, '2': 2, '3': 3, '4': 7, '5': 5 } },
+        UMI2: { count: { '1': 0, '2': 2, '3': 3, '4': 7, '5': 5 } },
+        UMI3: { count: { '1': 0, '2': 2, '3': 3, '4': 7, '5': 5 } },
+        UMI4: { count: { '1': 0, '2': 2, '3': 3, '4': 7, '5': 5 } },
+        UMI5: { count: { '1': 0, '2': 2, '3': 3, '4': 7, '5': 5 } },
+        UMI6: { count: { '1': 0, '2': 2, '3': 3, '4': 7, '5': 5 } },
+        deptAverage: {
+          UMI1: 1.23,
+          UMI2: 1.23,
+          UMI3: 1.23,
+          UMI4: 1.23,
+          UMI5: 1.23,
+          UMI6: 1.23,
+          year: 2014,
+          term: 'W2',
+          length: 41
+        }
+      }
+    ]
+    assert.deepEqual(addDeptData(courses, deptAvg), output)
+  })
 })
