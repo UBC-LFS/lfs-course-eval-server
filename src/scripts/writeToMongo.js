8eb57588384a4c78f44d273cf074bd42ac1b7db9
Justin Lee <justinlee0022@gmail.com>
2017-11-28 13:32:27 -0800

    fixed spacing

--- src/scripts/writeToMongo.js
+++ src/scripts/writeToMongo.js
@@ -11,22 +11,27 @@ jsonfile.readFile('./output/aggregatedData.json', (err, data) => {
   assert.equal(null, err)
   clearAndWrite(data, 'aggregatedData')
 })
+
 jsonfile.readFile('./output/coursePerformanceData.json', (err, data) => {
   assert.equal(null, err)
   clearAndWrite(data, 'CoursePerformance')
 })
+
 jsonfile.readFile('./output/enrolmentTrendData.json', (err, data) => {
   assert.equal(null, err)
   clearAndWrite(data, 'EnrolmentTrend')
 })
+
 jsonfile.readFile('./output/facultyAndDeptData.json', (err, data) => {
   assert.equal(null, err)
   clearAndWrite(data, 'facultyDeptData')
 })
+
 jsonfile.readFile('./output/overallInstructorData.json', (err, data) => {
   assert.equal(null, err)
   clearAndWrite(data, 'OverallInstructor')
 })
+
 jsonfile.readFile('./output/UMIInstructorData.json', (err, data) => {
   assert.equal(null, err)
   clearAndWrite(data, 'UMIInstructor')
