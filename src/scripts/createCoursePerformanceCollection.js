d777a796f327494836e3578c53ca613badc13367
Justin Lee <justinlee0022@gmail.com>
2017-11-28 11:32:32 -0800

    Added dept average to UMI instructor. Haven't tested yet, and also need to add in faculty average as well

--- src/scripts/createCoursePerformanceCollection.js
+++ src/scripts/createCoursePerformanceCollection.js
@@ -3,22 +3,22 @@ import jsonfile from 'jsonfile'
 import assert from 'assert'
 
 // currently faculty and dept averages are using the year's averages, not the terms. Maybe should change this to be terms later.
-const addDeptData = (instructorCourseRecords, deptData) =>
-  instructorCourseRecords.map(course => {
-    const deptFacultyRecord = R.find(deptRecord =>
-      R.keys(deptRecord).includes(course.year.toString()))(deptData)
-    course.facultyAverage = deptFacultyRecord[course.year].facultyAverage
-    course.deptAverage = deptFacultyRecord[course.year][course.dept + 'Average']
-    return course
-  }
-)
+// const addDeptData = (instructorCourseRecords, deptData) =>
+//   instructorCourseRecords.map(course => {
+//     const deptFacultyRecord = R.find(deptRecord =>
+//       R.keys(deptRecord).includes(course.year.toString()))(deptData)
+//     course.facultyAverage = deptFacultyRecord[course.year].facultyAverage
+//     course.deptAverage = deptFacultyRecord[course.year][course.dept + 'Average']
+//     return course
+//   }
+// )
 
-const aggregateCP = (instructorData, deptFacultyData) =>
-  instructorData.map(instructor => ({
-    PUID: instructor.PUID,
-    Courses: addDeptData(instructor.Courses, deptFacultyData)
-  })
-)
+// const aggregateCP = (instructorData, deptFacultyData) =>
+//   instructorData.map(instructor => ({
+//     PUID: instructor.PUID,
+//     Courses: addDeptData(instructor.Courses, deptFacultyData)
+//   })
+//   )
 
 // need to refactor this to use the generated JSON, will be tricky because I need to enforce the order
 // readDataByYear('2016', 'UMIInstructor', (res) => {
@@ -30,13 +30,41 @@ const aggregateCP = (instructorData, deptFacultyData) =>
 //   })
 // })
 
+const addDeptData = (courses, facultyDeptData) => {
+  return courses.map(course => {
+    const year = course.year
+    const term = course.term
+    const dept = course.dept
+
+    const deptData = facultyDeptData
+      .filter(x => x.department === dept)[0].data
+      .filter(x => x.year === year && x.term === term)[0]
+
+    course.deptAverage = deptData
+    // course.deptAverage = 
+    //   .filter(x => x.data.year === year && x.data.term === term)
+
+    return course
+  })
+}
+
+const addDeptAndFacultyAvgIntoUMIInstructorData = (UMIData, facultyDeptData) => {
+  return UMIData.map(instructor => {
+    return {
+      PUID: instructor.PUID,
+      Courses: addDeptData(instructor.Courses, facultyDeptData)
+    }
+  })
+}
+
 const outputCoursePerformance = () => {
-  jsonfile.readFile('./output/UMIInstructorData.json', (err, json) => {
+  jsonfile.readFile('./output/UMIInstructorData.json', (err, UMIData) => {
     assert.equal(null, err)
-    console.log(json)
-    jsonfile.readFile('./output/facultyAndDeptData.json', (err, json) => {
+    jsonfile.readFile('./output/facultyAndDeptData.json', (err, facultyDeptData) => {
       assert.equal(null, err)
-      console.log(json)
+      const result = addDeptAndFacultyAvgIntoUMIInstructorData(UMIData, facultyDeptData)
+      const file = './output/coursePerformanceCollection.json'
+      jsonfile.writeFile(file, result, (err) => assert.equal(err, null))
     })
   })
 }
