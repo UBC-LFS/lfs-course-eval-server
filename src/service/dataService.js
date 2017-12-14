e22cacd804e09c7a92017a5bbe2629fb92ec98a5
Justin Lee <justinlee0022@gmail.com>
2017-12-14 15:38:47 -0800

    Implemented overview calculation for selectedYear and previousYear

--- src/service/dataService.js
+++ src/service/dataService.js
@@ -10,6 +10,7 @@ import {
 } from '../utils/calculate'
 import { calculateEnrolment } from '../utils/aggregatedDataUtils'
 import * as collection from '../utils/constants'
+import R from 'ramda'
 
 const dataForOverallInstructor = year => {
   return new Promise((resolve, reject) => {
@@ -85,21 +86,33 @@ const dataForStats = (fromYear, toYear, dept) => {
   })
 }
 
-const overviewStats = data => {
-  const umi6Count = sumCount(data.map(section => section.UMI6.count))
+const overviewStats = (year, data) => {
+  const curYearSections = data.filter(section => section.year === Number(year))
+  const prevYearSections = data.filter(section => section.year !== Number(year))
+  
+  const curUMI6Count = sumCount(curYearSections.map(section => section.UMI6.count))
+  const prevUMI6Count = sumCount(prevYearSections.map(section => section.UMI6.count))
   return {
-    umi6: umiAvg(umi6Count),
-    enrolment: calculateEnrolment(data),
-    responseRate: toTwoDecimal(expandCount(umi6Count).length / calculateEnrolment(data)),
-    sections: data.length
+    currentYear: {
+      umi6: umiAvg(curUMI6Count),
+      enrolment: calculateEnrolment(curYearSections),
+      responseRate: toTwoDecimal(expandCount(curUMI6Count).length / calculateEnrolment(curYearSections)),
+      sections: curYearSections.length
+    },
+    previousYear: {
+      umi6: umiAvg(prevUMI6Count),
+      enrolment: calculateEnrolment(prevYearSections),
+      responseRate: toTwoDecimal(expandCount(prevUMI6Count).length / calculateEnrolment(prevYearSections)),
+      sections: prevYearSections.length
+    }
   }
 }
 
 const dataForOverview = (year) => {
-  const conditions = { year: Number(year) }
+  const conditions = { $or: [{ year: Number(year) }, { year: Number(year - 1) }] }
   return new Promise((resolve, reject) => {
     db.readData(collection.aggregatedData, conditions, (data) => {
-      if (data) resolve(overviewStats(data))
+      if (data) resolve(overviewStats(year, data))
       else reject(Error('db returned no result'))
     })
   })
