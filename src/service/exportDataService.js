74a61bb8b5259c45950fce5022d3d0947b6dd29d
Justin Lee <justinlee0022@gmail.com>
2017-12-18 11:10:00 -0800

    Fixed bug

--- src/service/exportDataService.js
+++ src/service/exportDataService.js
@@ -1,12 +1,5 @@
 import * as db from './dbService'
 import * as collection from '../utils/constants'
-import {
-  umiAvg,
-  expandCount,
-  sumCount,
-  toTwoDecimal
-} from '../utils/calculate'
-import { calculateEnrolment } from '../utils/aggregatedDataUtils'
 
 const dataForOptions = () => {
   return new Promise((resolve, reject) => {
@@ -17,39 +10,6 @@ const dataForOptions = () => {
   })
 }
 
-const overviewStats = (year, data) => {
-  const curYearSections = data.filter(section => section.year === Number(year))
-  const prevYearSections = data.filter(section => section.year !== Number(year))
-
-  const curUMI6Count = sumCount(curYearSections.map(section => section.UMI6.count))
-  const prevUMI6Count = sumCount(prevYearSections.map(section => section.UMI6.count))
-  return {
-    currentYear: {
-      umi6: umiAvg(curUMI6Count),
-      enrolment: calculateEnrolment(curYearSections),
-      responseRate: toTwoDecimal(expandCount(curUMI6Count).length / calculateEnrolment(curYearSections)),
-      sections: curYearSections.length
-    },
-    previousYear: {
-      umi6: umiAvg(prevUMI6Count),
-      enrolment: calculateEnrolment(prevYearSections),
-      responseRate: toTwoDecimal(expandCount(prevUMI6Count).length / calculateEnrolment(prevYearSections)),
-      sections: prevYearSections.length
-    }
-  }
-}
-
-const dataForOverview = (year) => {
-  const conditions = { $or: [{ year: Number(year) }, { year: Number(year - 1) }] }
-  return new Promise((resolve, reject) => {
-    db.readData(collection.aggregatedData, conditions, (data) => {
-      if (data) resolve(overviewStats(year, data))
-      else reject(Error('db returned no result'))
-    })
-  })
-}
-
 export {
-  dataForOptions,
-  dataForOverview
+  dataForOptions
 }
--- src/service/overviewDataService.js
+++ src/service/overviewDataService.js
@@ -5,8 +5,10 @@ import {
   standardDeviation,
   sumCount,
   percentFavourable,
-  dispersionIndex
+  dispersionIndex,
+  toTwoDecimal
 } from '../utils/calculate'
+import { calculateEnrolment } from '../utils/aggregatedDataUtils'
 import * as collection from '../utils/constants'
 
 const dataForOverallInstructor = year => {
@@ -83,6 +85,38 @@ const dataForStats = (fromYear, toYear, dept) => {
   })
 }
 
+const overviewStats = (year, data) => {
+  const curYearSections = data.filter(section => section.year === Number(year))
+  const prevYearSections = data.filter(section => section.year !== Number(year))
+
+  const curUMI6Count = sumCount(curYearSections.map(section => section.UMI6.count))
+  const prevUMI6Count = sumCount(prevYearSections.map(section => section.UMI6.count))
+  return {
+    currentYear: {
+      umi6: umiAvg(curUMI6Count),
+      enrolment: calculateEnrolment(curYearSections),
+      responseRate: toTwoDecimal(expandCount(curUMI6Count).length / calculateEnrolment(curYearSections)),
+      sections: curYearSections.length
+    },
+    previousYear: {
+      umi6: umiAvg(prevUMI6Count),
+      enrolment: calculateEnrolment(prevYearSections),
+      responseRate: toTwoDecimal(expandCount(prevUMI6Count).length / calculateEnrolment(prevYearSections)),
+      sections: prevYearSections.length
+    }
+  }
+}
+
+const dataForOverview = (year) => {
+  const conditions = { $or: [{ year: Number(year) }, { year: Number(year - 1) }] }
+  return new Promise((resolve, reject) => {
+    db.readData(collection.aggregatedData, conditions, (data) => {
+      if (data) resolve(overviewStats(year, data))
+      else reject(Error('db returned no result'))
+    })
+  })
+}
+
 export {
     dataForOverallInstructor,
     dataForUMIVSDispersion,
@@ -90,5 +124,6 @@ export {
     dataForCoursePerformance,
     dataForEnrolmentTrend,
     dataForFaculyAndDept,
-    dataForStats
+    dataForStats,
+    dataForOverview
 }
