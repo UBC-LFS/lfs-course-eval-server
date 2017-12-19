08944907cd62b46a0a3399a70ff0759fa4604de9
Justin Lee <justinlee0022@gmail.com>
2017-12-19 10:43:17 -0800

    Added test for filterByEnrolment

--- test/aggregatedDataUtils.test.js
+++ test/aggregatedDataUtils.test.js
@@ -3,6 +3,22 @@
 import assert from 'assert'
 import * as utils from '../src/utils/aggregatedDataUtils'
 
+describe('filterbByEnrolment', () => {
+  it('takes an array of sections and returns the sections with enrolment between lower and upper parameters', () => {
+    let input = [
+      { enrolment: 0 },
+      { enrolment: 10 },
+      { enrolment: 12 },
+      { enrolment: 24 }
+    ]
+    let output = [
+      { enrolment: 10 },
+      { enrolment: 12 }
+    ]
+    assert.deepEqual(utils.filterByEnrolment(10, 12)(input), output)
+  })
+})
+
 describe('calculateEnrolment', () => {
   it('takes an array of sections and returns the sum of the enrolments', () => {
     let input = [
