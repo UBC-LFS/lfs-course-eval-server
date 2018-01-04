0cb05588f34f6572f6fad8eb5de7cdac098337cb
Justin Lee <justinlee0022@gmail.com>
2018-01-04 11:05:42 -0800

    got rid of unused endpoint

--- src/routes/index.js
+++ src/routes/index.js
@@ -22,19 +22,4 @@ routes.use('/export', exportReport)
 
 routes.use('/instructor', instructor)
 
-routes.get('/list', (req, res, next) => {
-  const { title } = req.query
-
-  if (title == null || title === '') {
-    // You probably want to set the response HTTP status to 400 Bad Request
-    // or 422 Unprocessable Entity instead of the default 500 of
-    // the global error handler (e.g check out https://github.com/kbariotis/throw.js).
-    // This is just for demo purposes.
-    next(new Error('The "title" parameter is required'))
-    return
-  }
-
-  res.render('overview', { title })
-})
-
 export default routes
