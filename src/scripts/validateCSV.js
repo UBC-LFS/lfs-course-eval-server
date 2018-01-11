b05a2e0b9552fb83d88d8bc82c283469cc437f62
Justin Lee <justinlee0022@gmail.com>
2018-01-11 15:00:17 -0800

    Fixed error where dept and course dept did not match

--- src/scripts/validateCSV.js
+++ src/scripts/validateCSV.js
@@ -3,12 +3,9 @@ import readCSV from '../service/readCSV'
 import * as getFromCSV from './scriptUtils/getFromCSV'
 import assert from 'assert'
 
-readCSV('../scripts/source/course_eval_enrollments-2009-2017SA.csv', csv => {
+readCSV('../scripts/source/rawDataAll.csv', csv => {
   const noDeptMatch = csv.filter(ev => {
     return getFromCSV.getDept(ev) !== getFromCSV.getCourse(ev).split(' ')[0]
   })
-  console.log(noDeptMatch.filter(x => x.period === '2015W'))
-  console.log(noDeptMatch.length)
-  const incorrectPeriods = R.uniq(noDeptMatch.map(x => x.period))
-  console.log(incorrectPeriods)
+  console.log(noDeptMatch)
 })
