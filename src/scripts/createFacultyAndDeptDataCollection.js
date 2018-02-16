6fe87ec49de1e6572ed298ebc289da1ece1da82c
clarachu <cgc.chu@gmail.com>
2018-02-16 15:01:38 -0800

    CC - refactor create faculty and dept data collection

--- src/scripts/createFacultyAndDeptDataCollection.js
+++ src/scripts/createFacultyAndDeptDataCollection.js
@@ -4,6 +4,46 @@ import assert from 'assert'
 import jsonfile from 'jsonfile'
 import * as collection from '../utils/constants'
 
+const createTermCalculations = (object, data) => {
+  const splitIntoTerms = data.reduce((acc, section) => {
+    const year = section.year
+    const term = section.term
+    const key = year + term
+
+    const object = acc.find(obj => obj.hasOwnProperty(key))
+    if (object) {
+        object[key].push(section)
+    } else {
+        const newObj = {
+            [key]: [section]
+        }
+        acc.push(newObj)
+    }
+    return acc
+}, [])
+
+const dataObj = splitIntoTerms
+    .map(section => {
+        // console.log(Object.values(section)[0])
+        return Object.values(section)[0]
+    })
+    .map(sections => {
+        return {
+            'term': sections[0].term,
+            'year': sections[0].year,
+            'UMI1': umiAvg(sumCount(sections.map(section => section.UMI1.count))),
+            'UMI2': umiAvg(sumCount(sections.map(section => section.UMI2.count))),
+            'UMI3': umiAvg(sumCount(sections.map(section => section.UMI3.count))),
+            'UMI4': umiAvg(sumCount(sections.map(section => section.UMI4.count))),
+            'UMI5': umiAvg(sumCount(sections.map(section => section.UMI5.count))),
+            'UMI6': umiAvg(sumCount(sections.map(section => section.UMI6.count))),
+            'length': sections.length
+        }
+    })
+object.data = dataObj
+return object
+}
+
 const createAverageDept = data => {
     const departments = R.uniq(data
         .map(section => section.dept)
@@ -15,43 +55,7 @@ const createAverageDept = data => {
         const deptObj = {
             'department': deptSections[0].dept
         }
-        const splitIntoTerms = deptSections.reduce((acc, section) => {
-            const year = section.year
-            const term = section.term
-            const key = year + term
-
-            const object = acc.find(obj => obj.hasOwnProperty(key))
-            if (object) {
-                object[key].push(section)
-            } else {
-                const newObj = {
-                    [key]: [section]
-                }
-                acc.push(newObj)
-            }
-            return acc
-        }, [])
-
-        const dataObj = splitIntoTerms
-            .map(section => {
-                // console.log(Object.values(section)[0])
-                return Object.values(section)[0]
-            })
-            .map(sections => {
-                return {
-                    'term': sections[0].term,
-                    'year': sections[0].year,
-                    'UMI1': umiAvg(sumCount(sections.map(section => section.UMI1.count))),
-                    'UMI2': umiAvg(sumCount(sections.map(section => section.UMI2.count))),
-                    'UMI3': umiAvg(sumCount(sections.map(section => section.UMI3.count))),
-                    'UMI4': umiAvg(sumCount(sections.map(section => section.UMI4.count))),
-                    'UMI5': umiAvg(sumCount(sections.map(section => section.UMI5.count))),
-                    'UMI6': umiAvg(sumCount(sections.map(section => section.UMI6.count))),
-                    'length': sections.length
-                }
-            })
-        deptObj.data = dataObj
-        return deptObj
+        return createTermCalculations(deptObj, deptSections)
     })
     return calculateAverage
 }
@@ -60,43 +64,7 @@ const createAverageFaculty = data => {
     const facultyObj = {
         'department': 'faculty'
     }
-    const splitIntoTermsFaculty = data.reduce((acc, section) => {
-        const year = section.year
-        const term = section.term
-        const key = year + term
-
-        const object = acc.find(obj => obj.hasOwnProperty(key))
-        if (object) {
-            object[key].push(section)
-        } else {
-            const newObj = {
-                [key]: [section]
-            }
-            acc.push(newObj)
-        }
-        return acc
-    }, [])
-
-    const facultyDataObj = splitIntoTermsFaculty
-        .map(section => {
-            // console.log(Object.values(section)[0])
-            return Object.values(section)[0]
-        })
-        .map(sections => {
-            return {
-                'term': sections[0].term,
-                'year': sections[0].year,
-                'UMI1': umiAvg(sumCount(sections.map(section => section.UMI1.count))),
-                'UMI2': umiAvg(sumCount(sections.map(section => section.UMI2.count))),
-                'UMI3': umiAvg(sumCount(sections.map(section => section.UMI3.count))),
-                'UMI4': umiAvg(sumCount(sections.map(section => section.UMI4.count))),
-                'UMI5': umiAvg(sumCount(sections.map(section => section.UMI5.count))),
-                'UMI6': umiAvg(sumCount(sections.map(section => section.UMI6.count))),
-                'length': sections.length
-            }
-        })
-    facultyObj.data = facultyDataObj
-    return facultyObj
+    return createTermCalculations(facultyObj, data)
 }
 
 const createAverage = (json) => {
