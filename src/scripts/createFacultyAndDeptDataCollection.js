506e24e60b7923a7b74d7818782666a21ba5f2d1
Justin Lee <justinlee0022@gmail.com>
2018-02-16 15:19:21 -0800

    Slightly refactored createFacultyAndDeptDataCollection and test

--- src/scripts/createFacultyAndDeptDataCollection.js
+++ src/scripts/createFacultyAndDeptDataCollection.js
@@ -12,74 +12,66 @@ const createTermCalculations = (object, data) => {
 
     const object = acc.find(obj => obj.hasOwnProperty(key))
     if (object) {
-        object[key].push(section)
+      object[key].push(section)
     } else {
-        const newObj = {
-            [key]: [section]
-        }
-        acc.push(newObj)
+      const newObj = {
+        [key]: [section]
+      }
+      acc.push(newObj)
     }
     return acc
-}, [])
+  }, [])
 
-const dataObj = splitIntoTerms
-    .map(section => {
-        // console.log(Object.values(section)[0])
-        return Object.values(section)[0]
-    })
-    .map(sections => {
-        return {
-            'term': sections[0].term,
-            'year': sections[0].year,
-            'UMI1': umiAvg(sumCount(sections.map(section => section.UMI1.count))),
-            'UMI2': umiAvg(sumCount(sections.map(section => section.UMI2.count))),
-            'UMI3': umiAvg(sumCount(sections.map(section => section.UMI3.count))),
-            'UMI4': umiAvg(sumCount(sections.map(section => section.UMI4.count))),
-            'UMI5': umiAvg(sumCount(sections.map(section => section.UMI5.count))),
-            'UMI6': umiAvg(sumCount(sections.map(section => section.UMI6.count))),
-            'length': sections.length
-        }
-    })
-object.data = dataObj
-return object
+  const dataObj = splitIntoTerms
+    .map(section => Object.values(section)[0])
+    .map(sections => ({
+      'term': sections[0].term,
+      'year': sections[0].year,
+      'UMI1': umiAvg(sumCount(sections.map(section => section.UMI1.count))),
+      'UMI2': umiAvg(sumCount(sections.map(section => section.UMI2.count))),
+      'UMI3': umiAvg(sumCount(sections.map(section => section.UMI3.count))),
+      'UMI4': umiAvg(sumCount(sections.map(section => section.UMI4.count))),
+      'UMI5': umiAvg(sumCount(sections.map(section => section.UMI5.count))),
+      'UMI6': umiAvg(sumCount(sections.map(section => section.UMI6.count))),
+      'length': sections.length
+    }))
+  object.data = dataObj
+  return object
 }
 
 const createAverageDept = data => {
-    const departments = R.uniq(data
-        .map(section => section.dept)
-        )
-    const filteredByDept = departments
-        .map(dept => data
-            .filter(section => section.dept === dept))
-    const calculateAverage = filteredByDept.map(deptSections => {
-        const deptObj = {
-            'department': deptSections[0].dept
-        }
-        return createTermCalculations(deptObj, deptSections)
-    })
-    return calculateAverage
+  const departments = R.uniq(data.map(section => section.dept))
+  const filteredByDept = departments
+    .map(dept => data.filter(section => section.dept === dept))
+  const calculateAverage = filteredByDept.map(deptSections => {
+    const deptObj = {
+      'department': deptSections[0].dept
+    }
+    return createTermCalculations(deptObj, deptSections)
+  })
+  return calculateAverage
 }
 
 const createAverageFaculty = data => {
-    const facultyObj = {
-        'department': 'faculty'
-    }
-    return createTermCalculations(facultyObj, data)
+  const facultyObj = {
+    'department': 'faculty'
+  }
+  return createTermCalculations(facultyObj, data)
 }
 
-const createAverage = (json) => {
-    const deptResult = createAverageDept(json)
-    const facultyResult = createAverageFaculty(json)
-    return deptResult.concat(facultyResult)
+const createAverage = data => {
+  const deptResult = createAverageDept(data)
+  const facultyResult = createAverageFaculty(data)
+  return [...deptResult, facultyResult]
 }
 
 const outputFacultyDeptData = () => {
-    jsonfile.readFile('./output/' + collection.aggregatedData + '.json', (err, json) => {
-        assert.equal(null, err)
-        const file = './output/' + collection.facultyDeptData + '.json'
-        const result = createAverage(json)
-        jsonfile.writeFile(file, result, (err) => assert.equal(null, err))
-    })
+  jsonfile.readFile('./output/' + collection.aggregatedData + '.json', (err, json) => {
+    assert.equal(null, err)
+    const file = './output/' + collection.facultyDeptData + '.json'
+    const result = createAverage(json)
+    jsonfile.writeFile(file, result, (err) => assert.equal(null, err))
+  })
 }
 
 export {
