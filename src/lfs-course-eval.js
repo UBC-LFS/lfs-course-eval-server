46e74b5eec450858ff2f2e33915868aa6c446cec
jsondoo <jsondoo@users.noreply.github.com>
2018-05-02 13:05:32 -0700

    use port 8000 for local machines

--- src/lfs-course-eval.js
+++ src/lfs-course-eval.js
@@ -1,5 +1,5 @@
 import app from './app'
 require('dotenv').config()
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8000
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`)) // eslint-disable-line no-console
